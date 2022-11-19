70cfe79db05fe98a19a2b16159b51d2b39963869
Margarita Marmaridou <margmar@protonmail.com>
2022-11-19 17:14:06 +0200

    basic navbar

--- src/App.js
+++ src/App.js
@@ -1,12 +1,49 @@
-import './App.css';
+import "./App.css";
+//React Router
+import { Route, Routes } from "react-router-dom";
 //Components
-import Homepage from './components/Homepage';
+import Homepage from "./components/Homepage";
+import Navbar from "./components/Navbar";
+//MUI
+import { createTheme, colors, ThemeProvider } from "@mui/material";
 
 function App() {
+  const theme = createTheme({
+    palette: {
+      common: {
+        black: "#131b23",
+        white: "#FAF8F7",
+      },
+      primary: {
+        main: colors.deepOrange[500],
+        light: colors.deepOrange[300],
+        dark: colors.deepOrange[700],
+      },
+      error: {
+        main: colors.red[500],
+      },
+      success: {
+        main: colors.green[500],
+      },
+    },
+    typography: {
+      fontFamily: "Poppins",
+      fontWeightLight: 300,
+      fontWeightRegular: 400,
+      fontWeightMedium: 500,
+      fontWeightBold: 700,
+    },
+  });
+
   return (
-    <div className="App">
-        <Homepage />
-    </div>
+    <ThemeProvider theme={theme}>
+      <div className="App">
+        <Navbar />
+        <Routes>
+          <Route path="/" element={<Homepage />} />
+        </Routes>
+      </div>
+    </ThemeProvider>
   );
 }
 
